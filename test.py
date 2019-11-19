c6bd5234ef19de60d2735272ce16ca70fcd4377a
Catalina Vajiac <catvajiac@gmail.com>
2019-11-18 20:19:55 -0500

    finish compression unit tests

--- test.py
+++ test.py
@@ -10,50 +10,56 @@ import unittest
 
 def test_graph():
     g = nx.DiGraph()
-    g.add_edges_from([(1, 2), (2, 3), (3, 2), (3, 4), (4, 5), (3, 5)])
+    g.add_weighted_edges_from([('1', '2', 1), ('2', '3', 1), ('3', '2', 1), ('3', '4', 1), ('4', '5', 1), ('3', '5', 1)])
+    attributes = {node: {'contains': set([node])} for node in g.nodes}
+    nx.set_node_attributes(g, attributes)
     return g
 
 
 def test_graph_1():
     g = nx.DiGraph()
-    g.add_edges_from([(1, 2), (2, 1)])
+    g.add_weighted_edges_from([('1', '2', 1), ('2', '1', 1)])
+    attributes = {node: {'contains': set([node])} for node in g.nodes}
+    nx.set_node_attributes(g, attributes)
     return g
 
 def test_compressed_graph():
     g = nx.DiGraph()
-    g.add_edges_from([('1+2+3+4', 5), (5, 6)])
+    g.add_weighted_edges_from([('1+2+3+4', '5', 1), ('5', '6', 1)])
+    attributes = {node: {'contains': set([node])} for node in g.nodes}
+    attributes['1+2+3+4'] = {'contains': set(['1', '2', '3', '4'])}
+    nx.set_node_attributes(g, attributes)
     return g
 
 
-class TestCompression(unittest.testcase):
+class TestCompression(unittest.TestCase):
     def test_make_all_dirs(self):
         compression.make_all_dirs(['./test0{}'.format(x) for x in range(5)])
 
         for x in range(5):
             directory = './test0{}'.format(x)
             self.assertTrue(os.path.isdir(directory))
-            os.remove(directory)
+            os.rmdir(directory)
 
 
     def test_read_graph(self):
-        f, filename = tempfile.mkstemp()
-        for source, target in test_graph().edges:
-            f.write(" ".join(source, target, 1, 0 + '\n'))
-        for source, target in test_graph_1().edges:
-            f.write(" ".join(source, target, 1, 0 + '\n'))
-        f.close()
+        _, filename = tempfile.mkstemp()
+        with open(filename, 'w') as f:
+            for source, target in test_graph().edges:
+                f.write("{} {} {} {}".format(source, target, 1, 0) + "\n")
+            for source, target in test_graph_1().edges:
+                f.write("{} {} {} {}".format(source, target, 1, 1) + "\n")
 
-        g = read_graph(filename)
-
-        self.assertTrue(nx.is_isomorphic(g[0], test_graph()))
-        self.assertTrue(nx.is_isomorphic(g[1], test_graph_1()))
+        g = compression.read_graph(filename)
+        self.assertTrue(nx.is_isomorphic(g['0'], test_graph()))
+        self.assertTrue(nx.is_isomorphic(g['1'], test_graph_1()))
 
         os.remove(filename)
 
 
     def test_lambda_connection(self):
         g = test_graph()
-        source = g.nodes[0]
+        source = 1
 
         pr = nx.pagerank(g, personalization={source: 1})
 
@@ -73,9 +79,9 @@ class TestCompression(unittest.testcase):
         for supersource, supertarget in g.edges:
             for source in supersource.split('+'):
                 for target in supertarget.split('+'):
-                    self.assertTrue(source in g1)
-                    self.assertTrue(target in g1)
-                    self.assertTrue((source, target) in g1)
+                    self.assertTrue(source in g1.nodes)
+                    self.assertTrue(target in g1.nodes)
+                    self.assertTrue((source, target) in g1.edges)
 
 
     def test_is_edge(self):
@@ -87,10 +93,18 @@ class TestCompression(unittest.testcase):
 
     def test_graph_merge(self):
         g = test_graph()
-        g1 = compression.merge(g, 1, 2)
+        g = compression.init_compressed_graph(g)
+        g1 = compression.graph_merge(g, '1', '2')
         self.assertTrue('1+2' in g1.nodes)
-        for node in g.neighbors(1) + g.neighbors(2):
-            self.assertTrue(('1+2', node) in g1)
+        for node in g.neighbors('1'):
+            if node == '2':
+                continue
+            self.assertTrue(('1+2', node) in g1.edges)
+
+        for node in g.neighbors('2'):
+            if node == '1':
+                continue
+            self.assertTrue(('1+2', node) in g1.edges)
 
 
     def test_init_compressed_graph(self):
@@ -109,15 +123,15 @@ class TestCompression(unittest.testcase):
 
         self.assertEqual(compression.calc_cr(g0, g0), 1)
         self.assertEqual(compression.calc_cr(g1, g1), 1)
-        self.assertEqual(compression.calc_cr(g0, g1), len(g0.nodes)/len(g1.nodes))
+        self.assertEqual(compression.calc_cr(g0, g1), len(g1.nodes)/len(g0.nodes))
 
 
     def test_all_two_hop_pairs(self):
         g = test_graph()
-        pairs = compression.all_two_hop_pairs(g)
+        pairs = list(compression.all_two_hop_pairs(g))
 
-        pairs_in = [(1, 2), (1, 3), (2, 4), (3, 4), (3, 5), (4, 5)]
-        pairs_out = [(1, 5), (1, 4), (3, 1)]
+        pairs_in = [('1', '3'), ('2', '4'), ('2', '5'), ('3', '5')]
+        pairs_out = [('1', '5'), ('1', '4'), ('3', '1')]
 
         for pair in pairs_in:
             self.assertTrue(pair in pairs)
@@ -127,4 +141,16 @@ class TestCompression(unittest.testcase):
 
 
     def test_brute_force_greedy(self):
-        pass
+        g = test_graph()
+        g1 = compression.brute_force_greedy(g, cr=0.75)
+        expected_nodes = ['1+3+2', '4', '5']
+        expected_edges = [('4', '5'), ('1+3+2', '1+3+2')]
+        for node in g1.nodes:
+            self.assertTrue(node in expected_nodes)
+
+        for edge in g1.edges:
+            self.assertTrue(edge in expected_edges)
+
+
+if __name__ == '__main__':
+    unittest.main()
